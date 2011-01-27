01d77197574becfa59c4096e552eb0f7dcadffd6
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-01-27 11:52:16 +0900

    removed memcache-client

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ EnjuFlower::Application.config.session_store :cookie_store, :key => '_enju_flowe
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Enju1::Application.config.session_store :active_record_store
+# EnjuFlower::Application.config.session_store :active_record_store
