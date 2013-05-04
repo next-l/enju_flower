f19804de76c3e5bc02d77dedb8b4f800b013bf75
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-05 04:15:20 +0900

    fixed dummy ability

--- spec/dummy/app/models/ability.rb
+++ spec/dummy/app/models/ability.rb
@@ -36,8 +36,7 @@
           Realize,
           RealizeType,
           ResourceImportFile,
-          SeriesStatement,
-          SeriesStatementRelationship
+          SeriesStatement
         ]
         can :update, [
           ContentType,
@@ -95,8 +94,7 @@
           Produce,
           Realize,
           ResourceImportFile,
-          SeriesStatement,
-          SeriesStatementRelationship
+          SeriesStatement
         ]
         can :read, [
           CarrierType,
