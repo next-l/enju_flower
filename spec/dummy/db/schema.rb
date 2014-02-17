69326aacfa293dd09e4a69441c5b8793587de853
Kosuke Tanabe <kosuke@e23.jp>
2014-02-17 15:50:35 +0900

    updated dummy migration file

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -653,6 +653,7 @@ ActiveRecord::Schema.define(version: 20130519065837) do
     t.integer  "country_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "admin_networks"
     t.boolean  "allow_bookmark_external_url", default: false,                    null: false
     t.string   "url",                         default: "http://localhost:3000/"
   end
